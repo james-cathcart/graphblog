dc5fd2fb686711f047cf17fb89b79887025211b7
James Cathcart <cathcart.jm@gmail.com>
2024-06-11 10:42:50 -0600

    get users resolver method covered

--- graph/resolver_test.go
+++ graph/resolver_test.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+	"errors"
 	"github.com/99designs/gqlgen/client"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/james-cathcart/golog"
@@ -29,11 +30,17 @@ func TestResolver_GetUsers(t *testing.T) {
 		Users []*model.User `json:"users"`
 	}
 
+	type errResponse struct {
+		Message string   `json:"message"`
+		Path    []string `json:"path"`
+	}
+
 	// test data struct
 	type data struct {
 		articleRecords []*model.Article
 		records        []*model.User
 		query          string
+		errResp        *errResponse
 		resp           response
 		err            error
 	}
@@ -100,6 +107,29 @@ func TestResolver_GetUsers(t *testing.T) {
 				userSvcMock.EXPECT().GetAll().Return(mock.records, nil)
 			},
 		},
+		{
+			name: `user service returns error`,
+			input: data{
+				query: `query { users{ id name } }`,
+			},
+			mock: data{
+				err: errors.New(`some error`),
+			},
+			expect: data{
+				err: client.RawJsonError{
+					RawMessage: []byte(`[{"message":"some error","path":["users"]}]`),
+				},
+				errResp: &errResponse{
+					Message: `some error`,
+					Path: []string{
+						`users`,
+					},
+				},
+			},
+			prepare: func(articleSvcMock *article.MockService, userSvcMock *user.MockService, input data, mock data) {
+				userSvcMock.EXPECT().GetAll().Return(nil, mock.err)
+			},
+		},
 	}
 
 	// execute test cases
@@ -114,20 +144,54 @@ func TestResolver_GetUsers(t *testing.T) {
 
 			// execute logic
 			var resp response
-			c.MustPost(test.input.query, &resp)
+			err := c.Post(test.input.query, &resp)
 
-			// assertions
-			t.Log(`Response should be as expected`)
-			{
-				noErrors := true
-				for i := range test.expect.resp.Users {
-					if !reflect.DeepEqual(*test.expect.resp.Users[i], *resp.Users[i]) {
-						noErrors = false
-						t.Errorf("\tFAIL -> expected: %v, actual: %v", *test.expect.resp.Users[i], *resp.Users[i])
+			if test.expect.errResp != nil {
+
+				// handle negative cases
+				t.Log(`Error should be as expected`)
+				{
+					if !reflect.DeepEqual(test.expect.err, err) {
+						t.Errorf("\tFAIL -> expected: (%T|%v), actual: (%T|%v)", test.expect.err, test.expect.err, err, err)
+					} else {
+						t.Log("\tSuccess")
 					}
 				}
-				if noErrors {
-					t.Log("\tSuccess")
+
+				t.Log(`Users response should be nil`)
+				{
+					if resp.Users != nil {
+						t.Errorf("\tFAIL -> expected: nil, actual: %v", resp.Users)
+					} else {
+						t.Log("\tSuccess")
+					}
+				}
+
+			} else {
+
+				// handle positive cases
+
+				t.Log(`Error should be nil`)
+				{
+					if err != nil {
+						t.Errorf("\tFAIL -> expected: nil, actual: (%T|%v)", err, err)
+					} else {
+						t.Log("\tSuccess")
+					}
+				}
+
+				t.Log(`Response should be as expected`)
+				{
+					noErrors := true
+					for i := range test.expect.resp.Users {
+						if !reflect.DeepEqual(*test.expect.resp.Users[i], *resp.Users[i]) {
+							noErrors = false
+							t.Errorf("\tFAIL -> expected: %v, actual: %v", *test.expect.resp.Users[i], *resp.Users[i])
+						}
+					}
+					if noErrors {
+						t.Log("\tSuccess")
+					}
 				}
 			}
 		})
